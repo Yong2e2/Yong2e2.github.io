22d6d1888b0f3e5eb7007c6fd746c56e7b758f7d
Yong2e2 <kimsy7@clo-guard.com>
2025-02-25 14:50:52 +0900

    Fix: prefix 수정

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,13 @@ const withNextra = nextra({});
 
 // GitHub Pages를 위한 설정
 const isProduction = process.env.NODE_ENV === 'production';
+const prefix = isProduction ? 'Yong2e2.github.io' : '';
 
 export default withNextra({
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProduction ? '/' : '',
-  assetPrefix: isProduction ? '/' : '',
+  basePath: isProduction ? `/${prefix}` : '',
+  assetPrefix: isProduction ? `/${prefix}/` : '',
 });
