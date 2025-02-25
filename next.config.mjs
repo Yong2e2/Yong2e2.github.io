a04da51ab3306daa31687fe949c0a7f07a3605b1
Yong2e2 <kimsy7@clo-guard.com>
2025-02-25 14:26:21 +0900

    Fix: next config 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 import nextra from 'nextra';
 
 const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
   contentDirBasePath: '/',
 });
 
-// You can include other Next.js configuration options here, in addition to Nextra settings:
+// GitHub Pages를 위한 설정
+const isProduction = process.env.NODE_ENV === 'production';
+const repository = 'Yong2e2.github.io'; // 여기에 실제 레포지토리 이름을 넣으세요
+
 export default withNextra({
-  // ... Other Next.js config options
+  output: 'export', // 정적 내보내기 설정
+  images: {
+    unoptimized: true, // GitHub Pages에서는 이미지 최적화를 비활성화
+  },
+  basePath: isProduction ? `/${repository}` : '', // GitHub Pages를 위한 base path 설정
+  assetPrefix: isProduction ? `/${repository}/` : '', // 에셋 접두사 설정
 });
