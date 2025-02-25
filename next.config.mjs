11d06a72644f11453f625fb322784c7e7c1645bc
Yong2e2 <kimsy7@clo-guard.com>
2025-02-25 14:31:14 +0900

    Fix: next config 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -11,10 +11,9 @@ const isProduction = process.env.NODE_ENV === 'production';
 const repository = 'Yong2e2.github.io'; // 여기에 실제 레포지토리 이름을 넣으세요
 
 export default withNextra({
-  output: 'export', // 정적 내보내기 설정
   images: {
-    unoptimized: true, // GitHub Pages에서는 이미지 최적화를 비활성화
+    unoptimized: true,
   },
-  basePath: isProduction ? `/${repository}` : '', // GitHub Pages를 위한 base path 설정
-  assetPrefix: isProduction ? `/${repository}/` : '', // 에셋 접두사 설정
+  basePath: isProduction ? `/${repository}` : '',
+  assetPrefix: isProduction ? `/${repository}/` : '',
 });
