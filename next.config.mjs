de226e2a561c011003810d673fcb15ddccc0940c
Yong2e2 <kimsy7@clo-guard.com>
2025-02-25 14:42:06 +0900

    Feat: next-config 파일 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,15 @@
 import nextra from 'nextra';
 
-const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-  contentDirBasePath: '/',
-});
+const withNextra = nextra({});
 
 // GitHub Pages를 위한 설정
 const isProduction = process.env.NODE_ENV === 'production';
-const repository = 'Yong2e2.github.io'; // 여기에 실제 레포지토리 이름을 넣으세요
 
 export default withNextra({
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProduction ? `/${repository}` : '',
-  assetPrefix: isProduction ? `/${repository}/` : '',
+  basePath: isProduction ? '/' : '',
+  assetPrefix: isProduction ? '/' : '',
 });
